2f10c996b8b64d7a4c7eb34917e06b250920edc5
Jose Cruz <josedanielcrz@gmail.com>
2020-08-19 13:57:17 -0500

    Se corrige prueba de orm

--- test/compania/control/PruebaOrm.java
+++ test/compania/control/PruebaOrm.java
@@ -6,31 +6,43 @@ import org.junit.jupiter.api.Test;
 
 import compania.entidades.*;
 
-
-
-
+/**
+ * Clase donde se prueba el funcionamiento de la persistencia a través de un main
+ * @author Marco Contreras
+ * @version 1.0
+ */
 public class PruebaOrm {
 	
 	public static void main(String[] args) {
-	
-		
-		
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Compania");
 		EntityManager gestorBD = fabrica.createEntityManager();
+		EntityManager gestorBD2 = fabrica.createEntityManager();
+		EntityManager gestorBD3 = fabrica.createEntityManager();
+		
+		Empleado asalariado = new EmpleadoAsalariado("136", "Juan", 170000);
 		gestorBD.getTransaction().begin();
-		ControlCompania controlCompania = new ControlCompania();
-		controlCompania.adicionarEmpleado("123", "Juan", 170000, 0,0, 0, 'a');
-		controlCompania.adicionarEmpleado("124", "Camilo", 0, 20000,60, 0, 'h');
-		controlCompania.adicionarEmpleado("125", "Chepe", 160000, 0,0, 500000, 'c');
-		gestorBD.persist(controlCompania);
+		gestorBD.persist(asalariado);
 		gestorBD.getTransaction().commit();
-		Empleado buscarEmpleado = gestorBD.find(Empleado.class,"123");
-		Empleado buscarEmpleado2 = gestorBD.find(Empleado.class,"124");
-		Empleado buscarEmpleado3 = gestorBD.find(Empleado.class,"125");
+		Empleado buscarEmpleado = gestorBD.find(Empleado.class,"136");
+		gestorBD.close();
+		
+		Empleado porHora = new EmpleadoHora("137", "Camilo", 20000, 60);
+		gestorBD2.getTransaction().begin();
+		gestorBD2.persist(porHora);
+		gestorBD2.getTransaction().commit();
+		Empleado buscarEmpleado2 = gestorBD2.find(Empleado.class,"137");
+		gestorBD2.close();
+		
+		Empleado porComision = new EmpleadoComision("138", "Chepe", 160000, 500000);
+		gestorBD3.getTransaction().begin();
+		gestorBD3.persist(porComision);
+		gestorBD3.getTransaction().commit();
+		Empleado buscarEmpleado3 = gestorBD3.find(Empleado.class,"138");
+		gestorBD3.close();
+		
 		System.out.println(buscarEmpleado.getNombre());
 		System.out.println(buscarEmpleado2.getNombre());
 		System.out.println(buscarEmpleado3.getNombre());
-		gestorBD.close();
 	}
 	
 	
